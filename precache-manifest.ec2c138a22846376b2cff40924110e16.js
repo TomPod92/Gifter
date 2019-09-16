d317eec603ed2f2de0b211a9f097fe90a05f7e33
Tomek Podsiadlik <podsiadlik.tomek@gmail.com>
2019-09-16 14:28:31 +0200

    Updates

---
+++ precache-manifest.ec2c138a22846376b2cff40924110e16.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95630b9fb834f35dcd8342b41a5cf757",
+    "url": "/Gifter/index.html"
+  },
+  {
+    "revision": "e361a9be2819c2a3c8bd",
+    "url": "/Gifter/static/css/2.9918ec6b.chunk.css"
+  },
+  {
+    "revision": "0399b43bd9a9cd91e3b1",
+    "url": "/Gifter/static/css/main.1bf61552.chunk.css"
+  },
+  {
+    "revision": "e361a9be2819c2a3c8bd",
+    "url": "/Gifter/static/js/2.e8d51236.chunk.js"
+  },
+  {
+    "revision": "0399b43bd9a9cd91e3b1",
+    "url": "/Gifter/static/js/main.2a913075.chunk.js"
+  },
+  {
+    "revision": "a9c8d947322c6f008581",
+    "url": "/Gifter/static/js/runtime~main.a11cfd03.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Gifter/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Gifter/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Gifter/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Gifter/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "bcc35f8f903c73dca5534d4e205fe5ce",
+    "url": "/Gifter/static/media/sprite.bcc35f8f.svg"
+  }
+]);
